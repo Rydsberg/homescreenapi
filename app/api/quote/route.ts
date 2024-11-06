70b401e4a59bf15debe555f62c853f3bae5e0e50
Alve Svarén <alve.svaren@intunio.se>
2024-11-06 13:46:02 +0100

    Update route.ts

--- app/api/quote/route.ts
+++ app/api/quote/route.ts
@@ -10,9 +10,6 @@ export function GET() {
     case "4/4":
       quote = "May the fourth be with you!";
       break;
-    case "7/4":
-      quote = "Grattis Maja!";
-      break;
     case "24/12":
       quote = "God jul!";
       break;
@@ -23,7 +20,7 @@ export function GET() {
       quote = "3,14159265358979...";
       break;
     case "23/11":
-      quote = "0 1 1 2 3 5 8 13 21 34...";
+      quote = "1 1 2 3 5 8 13 21 34...";
       break;
   }
 
